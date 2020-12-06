ed5f74afb77995f418dcdea26eb47bdadbc1c701
spencercap <cappiello.spencer@gmail.com>
2020-12-06 14:46:59 +0000

    make dev tools extension work

--- src/devtools/main.ts
+++ src/devtools/main.ts
@@ -6,3 +6,7 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+const name = browser.i18n.getMessage('extName');
+browser.devtools.panels.create(name, '', 'devtools.html');
+// chrome.devtools.panels.create(name, '', 'devtools.html');
